6cf4ee82b1569a0b68a28e6f2f62dd9df60eace8
Stanisław Zagórowski <hasohasacz@gmail.com>
2024-05-16 18:48:14 +0200

    update hyprland config

--- wayland/hypr/scripts/zentoggle.sh
+++ wayland/hypr/scripts/zentoggle.sh
@@ -11,15 +11,13 @@ ZEN_ENABLED=$(cat /tmp/zen_enabled)
 
 if [ "$ZEN_ENABLED" = "0" ]; then
     echo "1" > /tmp/zen_enabled
-    killall hyprpaper
-    hyprctl keyword exec "hyprpaper --config $HOME/.config/hypr/hyprpaper-zen.conf"
+    hyprctl hyprpaper wallpaper ,~/Pictures/nvimbg.png
     eww kill
     $HOME/.config/eww/scripts/openzen.sh
     notify-send "Zen mode enabled"
 else
     echo "0" > /tmp/zen_enabled
-    killall hyprpaper
-    hyprctl keyword exec "hyprpaper --config $HOME/.config/hypr/hyprpaper.conf"
+    hyprctl hyprpaper wallpaper ,~/Pictures/campsite.jpg
     eww kill
     $HOME/.config/eww/scripts/open.sh
     notify-send "Zen mode disabled"
